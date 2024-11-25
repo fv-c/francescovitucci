d75859b0e1f1f9c318030a9d58b08ceca18ca2a6
Francesco Vitucci <121284991+fv-c@users.noreply.github.com>
2024-11-25 10:04:14 +0100

    Lun 25 Nov 2024 10:04:14 CET

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,1 @@
-$('.toggled-nav-wrapper').on('click', function(e){
-    e.stopPropagation();
-});
-$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
-    $('html').toggleClass('nav-open');
-});
+$(".toggled-nav-wrapper").on("click",function(o){o.stopPropagation()}),$(".nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay").on("click",function(){$("html").toggleClass("nav-open")});
